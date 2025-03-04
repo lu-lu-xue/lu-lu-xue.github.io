5c6128eac269c1ad69877a8cdf421d85ed8d76c2
Lulu Xue <llxue@ucdavis.edu>
2025-03-04 11:08:31 -0800

    update new info

---
+++ script.js
@@ -0,0 +1,8 @@
+// 这里可以编写你的 JavaScript 逻辑，或者留空
+
+console.log("Hello from script.js! 这是你的自定义脚本文件。");
+
+// 例如，如果你想让页面在加载时弹出一个提示，可以这么写：
+// window.onload = () => {
+//   alert("Welcome to my personal website!");
+// };
